663dbdc59f1c107fcea9b7137bec7c45d81eed8d
mkdyderski <Marcin.Dyderski@gmail.com>
2016-03-08 09:12:35 +0100

    eee

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -12,7 +12,7 @@
 #   Build and Reload Package:  'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
-
+#test line - ignore this comment
 hello <- function() {
   print("Hello, world!")
 }
